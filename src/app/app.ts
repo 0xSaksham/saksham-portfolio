860550a4764bde33a5c637f2c1ee2fe98199d52e
Saksham <sakshamgupta.ai@gmail.com>
2026-01-04 23:58:56 +0530

    ZardUI + startin page

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,35 @@
-import { Component, signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, signal, WritableSignal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+
+import { ZardButtonComponent } from '@/shared/components/button/button.component';
+
+type ViewState = 'LANDING' | 'ROLE_SELECTION' | 'GUEST_FLOW';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
+  viewState: WritableSignal<ViewState> = signal('LANDING');
+
+  startRecruiterFlow() {
+    this.viewState.set('ROLE_SELECTION');
+  }
+
+  startGuestFlow() {
+    this.viewState.set('GUEST_FLOW');
+  }
+
+  reset() {
+    this.viewState.set('LANDING');
+  }
+
+  setRole(role: string) {
+    console.log(`User Selected: ${role}`);
+  }
+
   protected readonly title = signal('saksham-portfolio');
 }
