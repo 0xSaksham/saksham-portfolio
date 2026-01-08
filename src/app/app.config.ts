6dfb9d905666824fb99580b124b9219681f2b6db
Saksham <sakshamgupta.ai@gmail.com>
2026-01-08 23:30:18 +0530

    fixed project tab issue

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,13 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withComponentInputBinding } from '@angular/router';
 
-import { routes } from './app.routes';import { provideZard } from '@/shared/core/provider/providezard';
-
+import { routes } from './app.routes';
+import { provideZard } from '@/shared/core/provider/providezard';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes),
-    provideZard(),]
+    provideRouter(routes, withComponentInputBinding()),
+    provideZard(),
+  ],
 };
